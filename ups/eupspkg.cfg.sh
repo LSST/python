42e5c9cff07ff438a1ecf4d7b70ef3c0e3cb9128
danielsf <scott.f.daniel@gmail.com>
2015-01-05 15:38:28 -0800

    attempt solution using sed

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,12 +3,16 @@ config()
 	# Verify that a compatible module can be found exists
 	./scripts/check_python -v | eups_console
 
-        echo "PYTHON DIR"
-        which python
-
 	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
 		die "Failed to find a compatible externally provided Python 2.7."
 	fi
+
+	dyldfallback = which python | sed 's/python//g' | sed 's/bin\///g'
+        envPrepend(DYLD_FALLBACK_LIBRARY_PATH, dyldfallback/lib)
+        echo 'DYLD_FALLBACK_LIBRARY_PATH'
+        echo $DYLDFALLBACK_LIBRARY_PATH
+
+
 }
 
 prep() { :; }
