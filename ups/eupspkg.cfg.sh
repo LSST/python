25f189602c786ac6c9096114668cd1676b571586
danielsf <scott.f.daniel@gmail.com>
2015-01-05 14:27:37 -0800

    silly print statements

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,6 +3,9 @@ config()
 	# Verify that a compatible module can be found exists
 	./scripts/check_python -v | eups_console
 
+        echo "PYTHON DIR"
+        echo $PYTHON_DIR
+
 	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
 		die "Failed to find a compatible externally provided Python 2.7."
 	fi
